648c3af0b25721738d2b98ad212345e18c0ee82d
Firhanabdillah <108823915+Firhanabdillah@users.noreply.github.com>
2023-05-01 01:24:52 +0700

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -1,8 +1,8 @@
 import streamlit as st
+from PIL import Image
 
 def app():
-    # judul halaman aplikasi 
     st.title("Aplikasi Prediksi Penyakit Jantung")
- # menambahkan gambar
+    st.write("Selamat datang di aplikasi prediksi penyakit jantung. Silakan gunakan navigasi di sebelah kiri untuk memilih halaman yang ingin Anda kunjungi.")
     image = Image.open("jantung.png")
-    st.image(image, caption="Gambar ilustrasi", use_column_width=True)
+    st.image(image, use_column_width=True)
