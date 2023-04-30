5698402c7ca6212bdc5035b843cc115942d06003
Firhanabdillah <108823915+Firhanabdillah@users.noreply.github.com>
2023-05-01 01:23:10 +0700

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -3,3 +3,6 @@ import streamlit as st
 def app():
     # judul halaman aplikasi 
     st.title("Aplikasi Prediksi Penyakit Jantung")
+ # menambahkan gambar
+    image = Image.open("jantung.png")
+    st.image(image, caption="Gambar ilustrasi", use_column_width=True)
